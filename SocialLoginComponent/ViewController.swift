672eb5a34ed55fbe1faa4d216596b7672d38c90e
Rishop Babu <rishop.babu@optisolbusiness.com>
2024-04-15 19:49:31 +0530

    Initial Commit

---
+++ SocialLoginComponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLoginComponent
+//
+//  Created by Mac-OBS-51 on 15/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
